4c98b24758e1192089defb715b3cbd866ec09ca7
aerobu <prabu.ganesh@gmail.com>
2024-10-31 12:21:01 -0700

    fixed display of IndiaMap

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import './maps.css';
 
 export const IndiaMap = () => {
     const geoUrl = "https://raw.githubusercontent.com/geohacker/india/master/state/india_telengana.geojson";
     return (
         <div className="map-container">
-            <h1>India States Political Leaning Map</h1>
-            <ComposableMap projection="geoMercator">
+            <ComposableMap 
+              projection="geoMercator" 
+              projectionConfig={{ scale: 900, center: [78.9629, 20.5937] }}
+              width={300} 
+              height={1000}
+            >
                 <Geographies geography={geoUrl}>
                     {({ geographies }) =>
                     geographies.map(geo => (
