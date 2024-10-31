65a6f09c201aa4709ecdfce3960fff8010a9c144
aerobu <prabu.ganesh@gmail.com>
2024-10-31 10:03:01 -0700

    changing app.jsx to include initial skeleton tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedTab, setSelectedTab] = useState('USA')
 
   return (
     <>
-      <h1>Election maps</h1>
+      <div className="App">
+        <div className="tabs">
+          <button>USA</button>
+          <button>India</button>
+        </div>
+      </div>
     </>
   )
 }
